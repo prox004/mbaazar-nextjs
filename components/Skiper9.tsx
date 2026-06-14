bcc30a1107f8a5d3079463b2106f53d17b8fbcac
Pro_X <bhandaryprotyoy@gmail.com>
2026-06-14 18:09:42 +0530

    feat: add global page transition preloader using Skiper9 component

--- components/Skiper9.tsx
+++ components/Skiper9.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import React, { memo, useState } from "react";
+import React, { memo, useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
 import Preloader from "@/app/components/Preloader";
+
 const Skiper9 = memo(() => {
+    const pathname = usePathname();
     const [showPreloader, setShowPreloader] = useState(true);
+
+    useEffect(() => {
+        setShowPreloader(true);
+    }, [pathname]);
 
     return (
         <AnimatePresence mode="wait">
